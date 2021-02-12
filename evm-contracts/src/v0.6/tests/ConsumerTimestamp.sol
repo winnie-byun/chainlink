31b63b6a13c439617ef44b8089b8e7a2ef509d9a
winnie-byun <winnie.byun@groundx.xyz>
2021-02-13 00:21:04 +0900

    remove unnecessary

--- evm-contracts/src/v0.6/tests/ConsumerTimestamp.sol
+++ evm-contracts/src/v0.6/tests/ConsumerTimestamp.sol
@@ -8,26 +8,22 @@ contract ConsumerTimestamp is ChainlinkClient {
   address internal oracleAddress; // Oracle Address that ChainLink listens to
   uint256[] public times;
 
-  event RequestFulfilled(
-    bytes32 indexed requestId,  // User-defined ID
-    uint256 indexed time
-  );
-
-  constructor (address _oracleAddress) public {
-    // specId = _specId;
-    specId = "0c63f2461b1e45b08d3b4957607e9b6d";
+  constructor (address _oracleAddress, address _link) public {
+    specId =  "8490a56c519149549f4625d742f48b1c";
     oracleAddress = _oracleAddress;
+    setChainlinkOracle(_oracleAddress);
+    setChainlinkToken(_link);
   }
 
   // requestTime requests time from off-chain
   function requestTime() public {
     Chainlink.Request memory req = buildChainlinkRequest(specId, address(this), this.setTime.selector);
-    sendChainlinkRequestTo(oracleAddress, req, 0);
+    req.add("httpgetwithunrestrictednetworkaccess", "http://localhost:5000");
+    sendChainlinkRequestTo(oracleAddress, req, 10);
   }
 
   // setTime is called from off-chain
   function setTime(bytes32 _requestId, uint256 t) public recordChainlinkFulfillment(_requestId) {
-    emit RequestFulfilled(_requestId, t);
     times.push(t);
   }
 
